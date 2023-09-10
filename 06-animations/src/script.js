6977e6426691775a690dd55e2b5d180d97985cba
Rajat Jaswal <rajatjaswal@Rajats-MacBook-Air.local>
2023-09-10 15:30:39 +0530

    learnt using deltaTime with Date.now() function

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -28,4 +28,25 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Time
+let time = Date.now();
+
+// Animations
+const tick = () => {
+    // Time
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+    // console.log(deltaTime, '======>deltaTime');
+    
+    // Update objects
+    mesh.rotation.y += 0.002 * deltaTime;
+
+    // Render
+    renderer.render(scene, camera)
+    
+    window.requestAnimationFrame(tick);
+}
+
+tick();
