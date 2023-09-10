01025701bf6de0d142113a78c046d47f857b3b66
Rajat Jaswal <rajatjaswal@Rajats-MacBook-Air.local>
2023-09-10 15:05:13 +0530

    learnt adding objects to the groups

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,27 +9,53 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00})
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff})
+);
+cube3.position.x = 2;
+group.add(cube3);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
 
 // Position
 // mesh.position.x = 0.7;
 // mesh.position.y = -0.6;
 // mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
-scene.add(mesh);
+// mesh.position.set(0.7, -0.6, 1);
+// scene.add(mesh);
 
 // Scale
 // mesh.scale.x = 2;
 // mesh.scale.y = 0.5;
 // mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
+// mesh.scale.set(2, 0.5, 0.5);
 
-// Rotation
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+// // Rotation
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
 
 // Doing the reorder after doesn't change the rotation as per reorder, so we need to use the reorder before to want the expected reordered rotation
 // mesh.rotation.reorder("YXZ");
@@ -37,7 +63,7 @@ mesh.rotation.y = Math.PI * 0.25;
 // Axes helper
 const axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
-    
+
 /**
  * Sizes
  */
@@ -55,7 +81,7 @@ camera.position.z = 3;
 // camera.position.x = 1;
 scene.add(camera);
 
-camera.lookAt(mesh.position);
+// camera.lookAt(mesh.position);
 
 /**
  * Renderer
