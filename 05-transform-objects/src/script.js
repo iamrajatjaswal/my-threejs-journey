3c664c33bc78cf8644375355f6fe82332a715f24
Rajat Jaswal <rajatjaswal@Rajats-MacBook-Air.local>
2023-09-10 14:52:49 +0530

    learnt transforming obects in threejs

---
+++ 05-transform-objects/src/script.js
@@ -0,0 +1,67 @@
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+// Position
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
+scene.add(mesh);
+
+// Scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+mesh.rotation.reorder("YXZ");
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+
+// Doing the reorder after doesn't change the rotation as per reorder, so we need to use the reorder before to want the expected reordered rotation
+// mesh.rotation.reorder("YXZ");
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+    
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+// camera.position.y = 1;
+// camera.position.x = 1;
+scene.add(camera);
+
+camera.lookAt(mesh.position);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
