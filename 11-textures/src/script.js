52410aecda20f9695a7407b40f8c57a9faa5f9e9
Rajat Jaswal <iamrajatjaswal@gmail.com>
2023-09-16 14:04:30 +0530

    11-textures learnt about textures, loading of textures, minFilter, magFilter, loadingManager

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -1,86 +1,207 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+/**
+ * Textures
+ */
+
+// Long way of doing it
+// const image = new Image();
+
+// const texture = new THREE.Texture(image);
+// console.log(texture, "texture before");
+
+// image.onload = () => {
+//   texture.needsUpdate = true;
+//   console.log(texture, "texture after");
+// };
+
+// image.src = "/textures/door/color.jpg";
+
+// Shorter faster way of loading a texture
+
+const loadingManager = new THREE.LoadingManager();
+
+/**
+ * Commenting the loading manager to see more console.log statements below
+ */
+// loadingManager.onStart = () => {
+//   console.log("loading started");
+// };
+
+// loadingManager.onLoad = () => {
+//   console.log("loading finished");
+// };
+
+// loadingManager.onProgress = () => {
+//   console.log("loading progressing");
+// };
+
+// loadingManager.onError = () => {
+//   console.log("loading error");
+// };
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+// const colorTexture = textureLoader.load("/textures/checkerboard-1024x1024.png");
+// const colorTexture = textureLoader.load("/textures/checkerboard-8x8.png");
+const colorTexture = textureLoader.load("/textures/minecraft.png");
+// const colorTexture = textureLoader.load("/textures/door/color.jpg");
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const heightTexture = textureLoader.load("/textures/door/height.jpg");
+const normalTexture = textureLoader.load("/textures/door/normal.jpg");
+const ambientOcclusionTexture = textureLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+
+
+// colorTexture.repeat.x = 2;
+// colorTexture.repeat.y = 3;
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping;
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping;
+// colorTexture.wrapS = THREE.RepeatWrapping;
+// colorTexture.wrapT = THREE.RepeatWrapping;
+
+// colorTexture.offset.x = 0.5;
+// colorTexture.offset.y = 0.5;
+
+// colorTexture.rotation = Math.PI * 0.25;
+// colorTexture.center.x = 0.5;
+// colorTexture.center.y = 0.5;
+
+colorTexture.generateMipmaps = false;
+colorTexture.minFilter = THREE.NearestFilter;
+
+// colorTexture.magFilter = THREE.NearestFilter;
+// colorTexture.magFilter = THREE.LinearFilter;
+
+/**
+ * Type of min filters (minFilter
+ */
+/*
+    *  THREE.NearestFilter
+    *  THREE.LinearFilter
+    *  THREE.NearestMinmapNearestFilter
+    *  THREE.NearestMinmapLinearFilter
+    *  THREE.LinearMinmapNearestFilter
+    *  THREE.LinearMinmapLinearFilter (default)
+
+
+/*
+
+/**
+ * Type of mag filters (magFilter)
+ */
+/*
+    *  THREE.NearestFilter
+    *  THREE.LinearFilter (default)
+
+
+/*
+
+
+// const texture = textureLoader.load(
+//   "/textures/door/color.jpg",
+//   () => {
+//     console.log("load");
+//   },
+//   () => {
+//     console.log("progress");
+//   },
+//   () => {
+//     console.log("error");
+//   }
+// );
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+// const geometry = new THREE.SphereBufferGeometry(1, 32, 32);
+// const geometry = new THREE.ConeBufferGeometry(1, 1, 32);
+// const geometry = new THREE.TorusBufferGeometry(1, 0.35, 32, 100);
+console.log(geometry.attributes)
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 1
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 1;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
